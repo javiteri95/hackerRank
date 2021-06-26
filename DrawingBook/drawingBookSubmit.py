1b09f84182715f43e25f5d27c58b07372d360974
Jose Viteri <josevns95@gmail.com>
2021-06-26 18:09:26 -0500

    Drawing book Python

---
+++ DrawingBook/drawingBookSubmit.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    # Write your code here
+    beggining_pages = p // 2
+    final_pages = (n + 1 - p) // 2 if n % 2 == 0 else (n - p) // 2
+
+    return min([beggining_pages, final_pages])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
